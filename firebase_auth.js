7c548d827c40f3e60709e21a57033c033e75c33d
vm-havsite <vivaanmehta0489@gmail.com>
2026-01-11 10:21:34 +0530

    Update firebase_auth.js

--- firebase_auth.js
+++ firebase_auth.js
@@ -127,7 +127,7 @@ function checkAuthState2(destination, onAuthenticated, onUnauthenticated) {
 	      }
         }
     });
-  }, 3000); // Wait exactly 1 second, then run once
+  }, 3000); // Wait exactly 3 second, then run once
 }
 
 export { signUp, signIn, signInWithGoogle, signOutUser, checkAuthState, checkAuthState2 };
