6152ed228d8bed780040fff7c7f8901b2bfad085
vm-havsite <vivaanmehta0489@gmail.com>
2026-05-03 12:43:26 +0530

    added styles

--- functions/articles/[id].js
+++ functions/articles/[id].js
@@ -78,6 +78,7 @@ function generateArticleHTML(article, thumbnail, articleId) {
     <title>${escapeHtml(title || 'Article')} - Havsite</title>
     <script src="/myscript.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/quill/1.3.7/quill.snow.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,700;0,900;1,700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     <style>
         :root {
