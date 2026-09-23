42dd6a138d8647d39a5fd01e55c49ffb518fee34
vm-havsite <vivaanmehta0489@gmail.com>
2026-09-23 09:50:41 +0530

    Update setUnreadCounterToZero.js

--- setUnreadCounterToZero.js
+++ setUnreadCounterToZero.js
@@ -1,4 +1,5 @@
 import { doc, setDoc } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
+import { db } from './firebase_auth.js';
 
 async function setUnreadCounterToZero(chatId, counterToUpdate){
   try{
